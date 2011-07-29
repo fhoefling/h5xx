c314682d91c7261544ad968595face9d32e3e1c4
Peter Colberg <peter.colberg@utoronto.ca>
2011-07-29 16:28:02 -0400

    Replace HALMD with h5xx in license headers.

--- test/h5xx.cpp
+++ test/h5xx.cpp
@@ -1,9 +1,9 @@
 /*
  * Copyright © 2010  Felix Höfling
  *
- * This file is part of HALMD.
+ * This file is part of h5xx.
  *
- * HALMD is free software: you can redistribute it and/or modify
+ * h5xx is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.

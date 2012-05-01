1c9230896eb4d3b1ab7b85e58afba36b8dbd1cce
Peter Colberg <pcolberg@chem.utoronto.ca>
2012-05-01 12:03:21 -0400

    cmake: use CMAKE_USER_MAKE_RULES_OVERRIDE to set default compiler flags
    This file allows setting compiler flags using CMAKE_*_INIT variables.

---
+++ cmake/platform.cmake
@@ -0,0 +1,19 @@
+set(CMAKE_BUILD_TYPE_INIT "Release")
+
+if(DEFINED CMAKE_CXX_COMPILER_ID)
+  if(CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
+
+    set(CMAKE_CXX_FLAGS_INIT "-fPIC -Wall")
+
+  elseif(CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
+
+    set(CMAKE_CXX_FLAGS_INIT "-fPIC -Wall")
+
+  elseif(CMAKE_CXX_COMPILER_ID STREQUAL "Intel")
+
+    set(CMAKE_CXX_FLAGS_INIT "-fPIC -Wall")
+
+  else()
+    message(WARNING "Unsupported CXX compiler: ${CMAKE_CXX_COMPILER_ID}")
+  endif()
+endif()

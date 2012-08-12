fhoefling/h5xx

commit 3ab9128d127aec9f220f6e8bae1b2ab065838139

Fix maximally inefficient dataset chunk size.
For a dataset tracing the evolution of a single scalar (e.g. double), a chunk
size of 1×1 (8 bytes) would cause significant meta-data overhead, which would
lead to increased file size, and memory exhaustion upon subsequently reading
the dataset. As an example, reading an extensible dataset of 10⁶ double values
(7.6 MiB) with h5py would cause a segmentation fault due to low memory, on a
machine with 2 GiB memory.

Ensure that the chunk size is at least 8 KiB, by multiplying the outermost
dimension of chunk size by a power of 2. This algorithm is far from perfect,
since it ignores the other dimensions of the chunk, which would need to be
adjusted to allow e.g. tracing a single particle over time in a trajectory
of many particles, without reading the trajectory of all particles.

The minimum chunk size of 8 kiB is recommended in [1].

[1] http://www.hdfgroup.org/training/HDFtraining/UsersGuide/Perform.fm2.html

Note that HDF5 has an internal chunk cache, by default of size 1 MiB.
The compression filter is only applied when the chunk is written from
the cache to disk, which means that even in the case of appending data
smaller than the chunk size, the chunk will only be compressed and
written to disk once.

For details on chunking, see Chunking in HDF5 [2].

[2] http://www.hdfgroup.org/HDF5/doc/_topic/Chunking/

h5xx/chunked_dataset.hpp
@@ -20,6 +20,7 @@
 #ifndef H5XX_CHUNKED_DATASET_HPP
 #define H5XX_CHUNKED_DATASET_HPP
 
+#include <algorithm>
 #include <boost/array.hpp>
 #include <boost/mpl/and.hpp>
 #include <boost/multi_array.hpp>
@@ -34,6 +35,10 @@
 namespace h5xx {
 
 namespace detail {
+
+// http://www.hdfgroup.org/training/HDFtraining/UsersGuide/Perform.fm2.html
+// It is recommended that the chunk size be at least 8K bytes.
+enum { CHUNK_MIN_SIZE = 8092 };
 
 /**
  * create chunked dataset 'name' in given group/file with given size
@@ -59,6 +64,12 @@ create_chunked_dataset(
     dim[0] = (max_size == H5S_UNLIMITED) ? 0 : max_size;
     max_dim[0] = max_size;
     chunk_dim[0] = 1;
+
+    // increase outermost dimension of chunk by powers of two until size of
+    // dataset is equal to or greater than recommended minimum chunk size
+    while (std::accumulate(chunk_dim.begin(), chunk_dim.end(), sizeof(T), std::multiplies<hsize_t>()) < CHUNK_MIN_SIZE) {
+        chunk_dim[0] *= 2;
+    }
 
     H5::DataSpace dataspace(dim.size(), &*dim.begin(), &*max_dim.begin());
     H5::DSetCreatPropList cparms;

commit d07d66ce43c6ec910ea823ac348f0986ce264ae6

Fix chunk size for fixed-sized dataset dimensions.
The chunk size may not exceed the maximum size of a fixed-size dataset.

This fixes a regression introduced with commit:3ab9128.

Running 1 test case...
Entering test suite "h5xx_chunked_dataset"
Entering test case "h5xx_chunked_dataset"
HDF5-DIAG: Error detected in HDF5 (1.8.9) thread 0:
  #000: H5D.c line 170 in H5Dcreate2(): unable to create dataset
    major: Dataset
    minor: Unable to initialize object
  #001: H5Dint.c line 430 in H5D__create_named(): unable to create and link to dataset
    major: Dataset
    minor: Unable to initialize object
  #002: H5L.c line 1638 in H5L_link_object(): unable to create new link to object
    major: Links
    minor: Unable to initialize object
  #003: H5L.c line 1882 in H5L_create_real(): can't insert link
    major: Symbol table
    minor: Unable to insert object
  #004: H5Gtraverse.c line 861 in H5G_traverse(): internal path traversal failed
    major: Symbol table
    minor: Object not found
  #005: H5Gtraverse.c line 641 in H5G_traverse_real(): traversal operator failed
    major: Symbol table
    minor: Callback failed
  #006: H5L.c line 1685 in H5L_link_cb(): unable to create object
    major: Object header
    minor: Unable to initialize object
  #007: H5O.c line 3015 in H5O_obj_create(): unable to open object
    major: Object header
    minor: Can't open object
  #008: H5Doh.c line 298 in H5O_dset_create(): unable to create dataset
    major: Dataset
    minor: Unable to initialize object
  #009: H5Dint.c line 1034 in H5D_create(): unable to construct layout information
    major: Dataset
    minor: Unable to initialize object
  #010: H5Dchunk.c line 432 in H5D_chunk_construct(): chunk size must be <= maximum dimension size for fixed-sized dimensions
    major: Dataset
    minor: Unable to initialize object
unknown location(0): fatal error in "h5xx_chunked_dataset": std::exception: failed to create dataset "array"

h5xx/chunked_dataset.hpp
@@ -71,6 +71,7 @@ create_chunked_dataset(
     while (std::accumulate(chunk_dim.begin(), chunk_dim.end(), sizeof(T), std::multiplies<hsize_t>()) < CHUNK_MIN_SIZE) {
         chunk_dim[0] *= 2;
     }
+    chunk_dim[0] = std::min(chunk_dim[0], max_dim[0]);
 
     H5::DataSpace dataspace(dim.size(), &*dim.begin(), &*max_dim.begin());
     H5::DSetCreatPropList cparms;
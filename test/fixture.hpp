7beb924ddecf9f7d47dbdf3adfe494168f6f8f60
Felix Höfling <hoefling@mf.mpg.de>
2014-01-24 15:58:56 +0100

    add test suite fixture "h5file"
    The fixture will allow us to automatically open and close test files at
    the beginning and end of each test case.

---
+++ test/fixture.hpp
@@ -0,0 +1,51 @@
+/*
+ * Copyright © 2013 Felix Höfling
+ *
+ * This file is part of h5xx.
+ *
+ * h5xx is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef TEST_FIXTURE_HPP
+#define TEST_FIXTURE_HPP
+
+#include <boost/test/unit_test_log.hpp>
+
+#include <h5xx/file.hpp>
+
+/**
+ * fixture that provides and cleans up an HDF5 file
+ */
+template <char const* filename>
+struct h5file
+{
+    h5file()
+      : file(filename)
+    {
+        BOOST_TEST_MESSAGE("HDF5 file created: " << filename);
+    }
+
+    ~h5file()
+    {
+        file.close();
+#ifdef NDEBUG
+        unlink(filename);
+#endif
+        BOOST_TEST_MESSAGE("HDF5 file removed: " << filename);
+    }
+
+    h5xx::file file;
+};
+
+#endif // ! TEST_FIXTURE_HPP

b6d520e16649a3f1ca0fd9159a4ae603e99dd560
Klaus Reuter <khr@rzg.mpg.de>
2014-11-27 11:25:09 +0100

    changed read_dataset<multi_array> to call/return by reference

--- h5xx/h5xx.hpp
+++ h5xx/h5xx.hpp
@@ -33,7 +33,7 @@ namespace h5xx {
 #include <h5xx/dataset.hpp>
 #include <h5xx/datatype.hpp>
 #include <h5xx/dataspace.hpp>
-#include <h5xx/hyperslab.hpp>
+#include <h5xx/slice.hpp>
 //#include <h5xx/chunked_dataset.hpp>
 #include <h5xx/exception.hpp>
 #include <h5xx/file.hpp>
--- h5xx/slice.hpp
+++ h5xx/slice.hpp
@@ -25,39 +25,109 @@
 #include <iterator>
 #include <vector>
 
-// TODO  slice -> slice
-
 namespace h5xx {
 
 class slice {
 public:
     slice();
 
+//   --- constructor with zero offset by default?
+//    template <class ArrayType>
+//    slice(ArrayType count);
+
     template <class ArrayType>
     slice(ArrayType offset, ArrayType count);
 
     template <class ArrayType>
+    slice(ArrayType offset, ArrayType count, ArrayType stride);
+
+    template <class ArrayType>
     slice(ArrayType offset, ArrayType count, ArrayType stride, ArrayType block);
 
+    int rank() const;
+
+    const std::vector<hsize_t> & get_count() const;
+    const std::vector<hsize_t> & get_offset() const;
+    const std::vector<hsize_t> & get_stride() const;
+    const std::vector<hsize_t> & get_block() const;
+
 private:
-    std::vector<int> offset_, count_, stride_, block_;
+    std::vector<hsize_t> offset_, count_, stride_, block_;
 };
+
+//template <class ArrayType>
+//slice::slice(ArrayType count)
+//{
+//    std::copy(count.begin(), count.end(), std::back_inserter(count_));
+//    offset_.assign(count.size(), 0);
+//    stride_.clear();
+//    block_.clear();
+//}
 
 template <class ArrayType>
 slice::slice(ArrayType offset, ArrayType count)
 {
+    if (offset.size() != count.size())
+    {
+        throw error("slice specification arrays must have identical length");
+    }
     std::copy(offset.begin(), offset.end(), std::back_inserter(offset_));
     std::copy(count.begin(),  count.end(),  std::back_inserter(count_));
+    stride_.clear();
+    block_.clear();
+}
+
+template <class ArrayType>
+slice::slice(ArrayType offset, ArrayType count, ArrayType stride)
+{
+    if ((offset.size() != count.size()) || (count.size() != stride.size()))
+    {
+        throw error("slice specification arrays must have identical length");
+    }
+    std::copy(offset.begin(), offset.end(), std::back_inserter(offset_));
+    std::copy(count.begin(),  count.end(),  std::back_inserter(count_));
+    std::copy(stride.begin(), stride.end(), std::back_inserter(stride_));
+    block_.clear();
 }
 
 template <class ArrayType>
 slice::slice(ArrayType offset, ArrayType count, ArrayType stride, ArrayType block)
 {
+    if ((offset.size() != count.size()) || (count.size() != stride.size()) || (stride.size() != block.size()))
+    {
+        throw error("slice specification arrays must have identical length");
+    }
     std::copy(offset.begin(), offset.end(), std::back_inserter(offset_));
     std::copy(count.begin(),  count.end(),  std::back_inserter(count_));
     std::copy(stride.begin(), stride.end(), std::back_inserter(stride_));
     std::copy(block.begin(),  block.end(),  std::back_inserter(block_));
 }
+
+int slice::rank() const
+{
+    return count_.size();
+}
+
+const std::vector<hsize_t> & slice::get_offset() const
+{
+    return offset_;
+}
+
+const std::vector<hsize_t> & slice::get_count() const
+{
+    return count_;
+}
+
+const std::vector<hsize_t> & slice::get_stride() const
+{
+    return stride_;
+}
+
+const std::vector<hsize_t> & slice::get_block() const
+{
+    return block_;
+}
+
 
 } // namespace h5xx
 

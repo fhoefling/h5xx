689057213ab2f1d8237cc42697de44460092489a
Felix Höfling <f.hoefling@fu-berlin.de>
2016-06-08 00:05:31 +0200

    release h5xx under the terms of the 3-clause BSD license
    All contributors have agreed to this change of the license, see
    https://github.com/fhoefling/h5xx/issues/3

--- test/file.cpp
+++ test/file.cpp
@@ -1,21 +1,12 @@
 /*
  * Copyright © 2013      Manuel Dibak
  * Copyright © 2013-2014 Felix Höfling
+ * All rights reserved.
  *
- * This file is part of h5xx.
+ * This file is part of h5xx — a C++ wrapper for the HDF5 library.
  *
- * h5xx is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * This software may be modified and distributed under the terms of the
+ * 3-clause BSD license.  See accompanying file LICENSE for details.
  */
 
 #define BOOST_TEST_MODULE h5xx_file
